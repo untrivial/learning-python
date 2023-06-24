2c30894bbd777db8ce223065918de3bce891988b
untrivial <axc@wharton.upenn.edu>
2023-06-24 17:59:49 -0400

    add convert_int_to_bin.py

---
+++ Intermediate/Data_Structures/Stack/convert_int_to_bin.py
@@ -0,0 +1,17 @@
+from stack import Stack
+import math
+
+def convert_int_to_bin(dec_num):
+    s = Stack()
+    binary_equiv = ""
+
+    while dec_num > 0:
+        s.push(dec_num % 2)
+        dec_num = int(math.floor(dec_num / 2))
+    while not s.is_empty():
+        bit = str(s.pop())
+        binary_equiv += bit
+    return binary_equiv
+
+print("hello")
+print(convert_int_to_bin(242))
