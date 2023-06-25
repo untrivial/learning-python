1d6aa1ef17a30a74e62d6e4b8c626d2d4cbf48ef
untrivial <axc@wharton.upenn.edu>
2023-06-24 20:39:33 -0400

    stuff

---
+++ Intermediate/Data_Structures/Singly_Linked_List/singly_linked_list.py
@@ -0,0 +1,26 @@
+"""
+Singly Linked List
+
+Insertion/Deletion: O(1)
+Append: O(n)
+Accessing Elements: O(n)
+"""
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
