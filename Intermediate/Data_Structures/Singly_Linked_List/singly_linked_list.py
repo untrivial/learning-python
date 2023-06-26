6a9313fecb092caaa0b5ad08095dbd61186bcf33
untrivial <axc@wharton.upenn.edu>
2023-06-25 22:32:50 -0400

    update singly linked list

--- Intermediate/Data_Structures/Singly_Linked_List/singly_linked_list.py
+++ Intermediate/Data_Structures/Singly_Linked_List/singly_linked_list.py
@@ -11,9 +11,16 @@ class Node:
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
 
     def append(self, data):
         new_node = Node(data)
@@ -24,3 +31,214 @@ class LinkedList:
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+        self.head = new_node
+    
+    def insert_after_node(self, prev_node, data):
+        if not prev_node:
+            print("Previous node does not exist.")
+            return
+        new_node = Node(data)
+        
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key): # Deletion by Value
+
+        cur_node = self.head
+
+        if cur_node and cur_node.data == key: # if key at head
+            self.head = cur_node.next
+            cur_node = None
+            return
+        
+        prev = None
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+        
+        if cur_node is None:
+            return
+        
+        prev.next = cur_node.next
+        cur_node = None
+    
+    def delete_node_at_pos(self, pos):
+        if self.head:
+            cur_node = self.head
+            if pos == 0:
+                self.head = cur_node.next
+                cur_node = None
+                return
+            
+            prev = None
+            count = 0
+            while cur_node and count != pos:
+                prev = cur_node
+                cur_node = cur_node.next
+                count += 1
+
+            if cur_node is None:
+                return
+            
+            prev.next = cur_node.next
+            cur_node = None
+    
+    def len_iterative(self):
+        count = 0
+        cur_node = self.head
+        while cur_node:
+            count += 1
+            cur_node = cur_node.next
+        return count
+    
+    def len_recursive(self, node): # pass first node as parameter
+        if node is None:
+            return 0
+        return 1 + self.len_recursive(node.next)
+    
+    def swap_nodes(self, key_1, key_2):
+
+        if key_1 == key_2:
+            return
+
+        prev_1 = None
+        curr_1 = self.head
+        while curr_1 and curr_1.data != key_1:
+            prev_1 = curr_1
+            curr_1 = curr_1.next
+        
+        prev_2 = None
+        curr_2 = self.head
+        while curr_2 and curr_2.data != key_2:
+            prev_2 = curr_2
+            curr_2 = curr_2.next
+        
+        if not curr_1 or not curr_2:
+            return
+        
+        if prev_1:
+            prev_1.next = curr_2
+        else:
+            self.head = curr_2
+        
+        if prev_2:
+            prev_2.next = curr_1
+        else:
+            self.head = curr_1
+
+        curr_1.next, curr_2.next = curr_2.next, curr_1.next
+
+    def reverse_iterative(self):
+        prev = None
+        cur = self.head
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        self.head = prev
+    
+    def reverse_recursive(self):
+
+        def _reverse_recursive(cur, prev):
+            if not cur:
+                return prev
+            
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+            return _reverse_recursive(cur, prev)
+        
+        self.head = _reverse_recursive(cur=self.head, prev=None)
+    
+    def merge_sorted(self, llist):
+
+        p = self.head
+        q = llist.head
+        s = None
+        
+        if not p:
+            return q
+        if not q:
+            return p
+        
+        if p and q:
+            if p.data <= q.data:
+                s = p
+                p = s.next
+            else:
+                s = q
+                q = s.next
+            new_head = s
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+        if not p:
+            s.next = q
+        if not q:
+            s.next = p
+
+        self.head = new_head
+        return self.head
+    
+    def remove_duplicates(self):
+        cur = self.head
+        prev = None
+        dup_values = dict()
+
+        while cur:
+            if cur.data in dup_values:
+                prev.next = cur.next
+                cur = None
+            else:
+                dup_values[cur.data] = 1
+                prev = cur
+            cur = prev.next
+
+    def print_nth_from_last(self, n):
+        total_len = self.len_iterative()
+
+        cur = self.head
+        while cur:
+            if total_len == n:
+                print(cur.data)
+                return cur.data
+            total_len -= 1
+            cur = cur.next
+        if cur is None:
+            return
+        
+    def print_nth_from_last_2(self, n):
+        p = self.head
+        q = self.head
+
+        if n > 0:
+            count = 0
+            while q: # set q ahead by n places
+                count += 1
+                if(count>=n):
+                    break
+                q = q.next
+        
+            if not q:
+                print(str(n) + " is greater than the number of nodes in list.")
+                return
+            
+            while p and q.next: # iterate until q reaches None
+                p = p.next
+                q = q.next
+            return p.data
+        else:
+            return None
