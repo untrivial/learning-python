7514087d7d62ed5272e67f7ce70820d71d2d264f
untrivial <axc@wharton.upenn.edu>
2023-06-29 21:10:44 -0400

    m

---
+++ Intermediate/Data_Structures/Circular_Linked_List/circular_linked_list.py
@@ -0,0 +1,81 @@
+'''
+Circular Linked List
+'''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def prepend(self, data):
+        pass
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            self.head.next = self.head
+        else: 
+            new_node = Node(data)
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+            new_node.next = self.head
+    
+    def print_list(self):
+        cur = self.head
+
+        while cur:
+            print(cur.data)
+            cur = cur.next
+            if cur == self.head:
+                break
+    
+    def prepend(self, data):
+        new_node = Node(data)
+        cur = self.head
+        new_node.next = self.head
+
+        if not self.head:
+            new_node.next = new_node
+        else:
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+        self.head = new_node
+
+    def remove(self, key):
+        if self.head:
+            if self.head.data == key:
+                cur = self.head
+                while cur.next != self.head:
+                    cur = cur.next
+                if self.head == self.head.next:
+                    self.head = None # if self.head is the only node
+                else:
+                    cur.next = self.head.next
+                    self.head = self.head.next
+            else:
+                cur = self.head
+                prev = None
+                while cur.next != self.head:
+                    prev = cur
+                    cur = cur.next
+                    if cur.data == key:
+                        prev.next = cur.next
+                        cur = cur.next
+    
+    def __len__(self):
+        cur = self.head
+        count = 0
+        while cur:
+            count += 1
+            cur = cur.next
+            if cur == self.head:
+                break
+        return count
--- Intermediate/Data_Structures/Singly_Linked_List/singly_linked_list.py
+++ Intermediate/Data_Structures/Singly_Linked_List/singly_linked_list.py
@@ -242,3 +242,131 @@ class LinkedList:
             return p.data
         else:
             return None
+        
+    def count_occurences_iterative(self, data):
+        count = 0
+        cur = self.head
+        while cur:
+            if cur.data == data:
+                count += 1
+            cur = cur.next
+        return count
+    
+    def count_occurences_recursive(self, node, data):
+        if not node:
+            return 0
+        if node.data == data:
+            return 1 + self.count_occurences_recursive(node.next, data)
+        else:
+            return self.count_occurences_recursive(node, data)
+        
+    def rotate(self, k):
+        if self.head and self.head.next:
+            p = self.head
+            q = self.head
+            prev = None
+            count = 0
+
+            while p and count < k:
+                prev = p
+                p = p.next
+                q = q.next
+                count += 1
+            p = prev
+            while q:
+                prev = q
+                q = q.next
+            q = prev
+
+            q.next = self.head
+            self.head = p.next
+            p.next = None
+
+    def is_palindrome(self): # string method
+        s = ""
+        p = self.head
+        while p:
+            s += p.data
+            p = p.next
+        return s == s[::-1]
+    
+    def is_palindrome_2(self): # stack method
+        p = self.head
+        s = []
+        while p:
+            s.append(p.data)
+            p = p.next
+        p = self.head
+        while p:
+            data = s.pop()
+            if p.data != data:
+                return False
+            p = p.next
+        return True
+    
+    def is_palindrome_3(self): # two pointers
+        if self.head:
+            p = self.head
+            q = self.head
+            prev = []
+
+            i = 0
+            while q:
+                prev.append(q)
+                q = q.next
+                i += 1
+            q = prev[i-1]
+
+            count = 1
+
+            while count <= i//2 + 1:
+                if prev[-count].data != p.data:
+                    return False
+                p = p.next
+                count += 1
+            return True
+        else:
+            return True
+        
+    def move_tail_to_head(self):
+        if self.head:
+            last = self.head
+            second_to_last = None
+            while last.next:
+                second_to_last = last
+                last = last.next
+            last.next = self.head
+            second_to_last.next = None
+            self.head = last
+    
+    def sum_two_lists(self, llist):
+        p = self.head
+        q = llist.head
+
+        sum_llist = LinkedList()
+
+        carry = 0
+        while p or q:
+            if not p:
+                i = 0
+            else:
+                i = p.data
+            if not q:
+                j = 0
+            else:
+                j = q.data
+            
+            s = i + j + carry
+            if s >= 10:
+                carry = 1
+                remainder = s % 10
+                sum_llist.append(remainder)
+            else:
+                carry = 0
+                sum_llist.append(s)
+
+            if p:
+                p = p.next
+            if q:
+                q = q.next
+        return sum_llist
