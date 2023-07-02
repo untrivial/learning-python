606f3beb6eb30fc95b65499309a4e7510302e806
untrivial <axc@wharton.upenn.edu>
2023-07-02 15:09:09 -0400

    finish circ list

--- Intermediate/Data_Structures/Circular_Linked_List/circular_linked_list.py
+++ Intermediate/Data_Structures/Circular_Linked_List/circular_linked_list.py
@@ -13,7 +13,17 @@ class CircularLinkedList:
         self.head = None
     
     def prepend(self, data):
-        pass
+        new_node = Node(data)
+        cur = self.head
+        new_node.next = self.head
+
+        if not self.head:
+            new_node.next = new_node
+        else:
+            while cur.next != self.head:
+                cur = cur.next
+            cur.next = new_node
+        self.head = new_node
 
     def append(self, data):
         if not self.head:
@@ -79,3 +89,79 @@ class CircularLinkedList:
             if cur == self.head:
                 break
         return count
+    
+    def split_list(self):
+        size = len(self)
+
+        if size == 0:
+            return None
+        if size == 1:
+            return self.head
+        
+        mid = size//2
+        count = 0
+
+        prev = None
+        cur = self.head
+
+        while cur and count < mid:
+            count += 1
+            prev = cur
+            cur = cur.next
+        prev.next = self.head
+
+        split_cllist = CircularLinkedList()
+        while cur.next != self.head:
+            split_cllist.append(cur.data)
+            cur = cur.next
+        split_cllist.append(cur.data)
+
+        self.print_list()
+        print("\n")
+        split_cllist.print_list()
+    
+    def remove_node(self, node):
+        if self.head == node:
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+            if self.head == self.head.next:
+                self.head = None
+            else:
+                cur.next = self.head.next
+                self.head = self.head.next
+        else:
+            cur = self.head
+            prev = None
+            while cur.next != self.head:
+                prev = cur
+                cur = cur.next
+                if cur == node:
+                    prev.next = cur.next
+                    cur = cur.next
+    
+    def josephus_circle(self, step):
+        cur = self.head
+
+        length = len(self)
+        while length > 1:
+            count = 1
+            while count != step:
+                cur = cur.next
+                count += 1
+            print("KILL:" + str(cur.data))
+            self.remove_node(cur)
+            cur = cur.next
+            length -= 1
+
+    def is_circular_linked_list(self, input_list):
+        if input_list.head:
+            cur = input_list.head
+            while cur.next:
+                cur = cur.next
+                if cur.next == input_list.head:
+                    return True
+            return False
+        else:
+            return False
+        
