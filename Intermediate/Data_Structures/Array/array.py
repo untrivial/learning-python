7202ac846db8e28884840adf02931f3695dd4b7a
untrivial <axc@wharton.upenn.edu>
2023-07-03 18:06:56 -0400

    array

---
+++ Intermediate/Data_Structures/Array/array.py
@@ -0,0 +1,66 @@
+# Is it possible to advance from the start of the array to the last element
+# given that the maximum you can advance from a position is based on
+# the value of the array at the index you are currently present on?
+
+def array_advance(A):
+    furthest_reached = 0
+    last_idx = len(A) - 1
+    i = 0
+    while i <= furthest_reached and furthest_reached < last_idx:
+        furthest_reached = max(furthest_reached, A[i] + i)
+        i += 1
+    return furthest_reached >= last_idx
+
+
+# Given an array that represents a decimal integer, add one to the integer.
+
+def plus_one(A):
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i-1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append[0]
+    return A
+
+
+# Given an array of integers, return True or False if the array 
+# has two numbers that add up to a specific target
+
+# Brute-force approach, O(n^2) time and O(1) space
+def two_sum_brute_force(A, target):
+    for i in range(len(A) - 1):
+        for j in range(i + 1, len(A)):
+            if A[i] + A[j] == target:
+                print(A[i], A[j])
+                return True
+    return False
+
+# Hash table approach, O(n) time and O(n) space
+def two_sum_hash_table(A, target):
+    ht = dict()
+    for i in range(len(A)):
+        if A[i] in ht:
+            print(ht[A[i]], A[i])
+            return True
+        else:
+            ht[target - A[i]] = A[i]
+    return False
+
+# Pair of indices approach, O(n) time and O(1) complexity
+# Assumes array is sorted
+def two_sum(A, target):
+    i = 0
+    j = len(A) - 1
+    while i < j:
+        if A[i] + A[j] == target:
+            print(A[i], A[j])
+            return True
+        elif A[i] + A[j] < target:
+            i += 1
+        else:
+            j -= 1
+    return False
